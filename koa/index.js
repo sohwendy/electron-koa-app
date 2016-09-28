957d3a2eb8bf00771f4a1ea68472ac17393a6f47
Wendy Soh <sohwendy@gmail.com>
2016-09-28 14:08:26 +0800

    2nd not working commit

--- koa/index.js
+++ koa/index.js
@@ -4,11 +4,11 @@ const render = require('koa-ejs');
 const serve = require('koa-static');
 const path = require('path');
 const fs = require('co-fs');
-const $ = require('jQuery');
-const formidable = require('koa-formidable')
+//const formidable = require('koa-formidable');
+const body = require('koa-body')({ multipart: true,  uploadDir: path.join(__dirname, 'uploads'), keepExtensions: true });
 const koaApp = koa();
 
-
+// koaApp.use(body);
 koaApp.use(serve('public'));
 
 render(koaApp, {
@@ -19,32 +19,83 @@ render(koaApp, {
   debug: true
 });
 
-// const defaultdevice = {
-//   users: []
-// };
+var defaultDevice = {
+  id: '',
+  name: '',
+  brand: '',
+  model: '',
+  os: '',
+  users: [],
+  date: '',
+  file: { path: ''}
+}
 
 var devices =
   [{
-    name: 'name',
-    brand: 'brand',
+    id: 0,
+    name: 'name 1',
+    brand: 'apple',
     model: 'model',
-    os: 'os',
-    users: ['Showcase', 'Project'],
+    os: 'ios',
+    users: ['demo', 'project'],
     date: '25 Sept 2016'
   }, {
+    id: 1,
     name: 'name 2',
-    brand: 'brand 2',
+    brand: 'lg',
     model: 'model 2',
-    os: 'os 2',
-    users: ['Showcase', 'Project'],
+    os: 'android',
+    users: ['demo', 'project'],
     date: '26 Sept 2016'
   }];
+
+const OS =
+  [{
+    label: 'Android',
+    value: 'android'
+  }, {
+    label: 'iOS',
+    value: 'ios'
+  }, {
+    label: 'Other OS',
+    value: 'others'
+  }];
+
+const USERS =
+  [{
+    label: 'Usability Lab',
+    value: 'ux'
+  }, {
+    label: 'Showcase',
+    value: 'demo'
+  }, {
+    label: 'Project',
+    value: 'project'
+  }];
+
+const BRANDS =
+  [{
+    label: 'Apple',
+    value: 'apple'
+  }, {
+    label: 'LG',
+    value: 'lg'
+  }, {
+    label: 'Samsung',
+    value: 'samsung'
+  }];
+
+DISPLAY = {
+  os: OS,
+  users: USERS,
+  brand: BRANDS
+};
 
 
 router
   .get('/devices',
     function *(next) {
-      yield this.render("main", { devices: devices });
+      yield this.render("main", { devices: devices, display: DISPLAY });
     })
   .get('/devices/:id',
     function *(next) {
@@ -52,31 +103,57 @@ router
       this.id = this.params.id;
       yield next;
     },
-    function *hello(next) {
-      yield this.render("device", { device: devices[this.id] });
+    function *(next) {
+      yield this.render("device", { device: devices[this.id], display: DISPLAY });
+    })
+  .get('/devices/:id/edit',
+    function *(next) {
+      this.id = this.params.id;
+      yield next;
+    },
+    function *(next) {
+      yield this.render("form", { device: devices[this.id], display: DISPLAY });
     })
   .get('/devices/new',
     function *(next) {
-      console.log('fffffff');
-      yield this.render("form");
+      yield this.render("form", { device: defaultDevice, display: DISPLAY});
     })
   .post('/devices/create',
+    body,
+    processFile,
     function *(next) {
-      const options = { uploadDir: path.join(__dirname, 'uploads'), keepExtensions: true };
+      requestBody = this.request.body;
+      // var photo = requestBody.files.photo;
+      // if (photo.size > 0) {
+      //   yield fs.rename(photo.path, path.join(__dirname, 'uploads', photo.name))
+      // }
 
-      const form = yield formidable.parse(options, this);
+      var id = devices.length;
+      var device = { id: id, file: this.file };
+      //var device = { id: id, file: { path: path.join(__dirname, 'uploads', photo.name) } };
+      device = Object.assign(device, requestBody.fields);
+      Array.isArray(device) ? device.users = [device.users] : null;
 
-      var file = form.files.photo;
-      var image = yield fs.rename(file.path, path.join(__dirname, 'uploads', file.name));
+      devices.push(device);
 
-      // devices.push(Object.assign({}, defaultdevice, form.fields));
-      // console.log('======================================================');
-      console.log(image);
+      console.log(devices);
+      yield this.render("device", { device: devices[id], display: DISPLAY });
+    });
 
-      // devices.push(form.fields);
-      // console.log(devices);
-    })
-  ;
+
+
+
+function *processFile(next) {
+
+  console.log(this.request.body);
+
+  var photo = this.request.body.files.photo;
+  if (photo.size > 0) {
+    yield fs.rename(photo.path, path.join(__dirname, 'uploads', photo.name))
+  }
+  this.file = { path: path.join(__dirname, 'uploads', photo.name) };
+
+}
 
 koaApp.use(router.routes());
 
